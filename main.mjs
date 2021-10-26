bd22ef72dcf23f1f5871eed794c97bd29e94ad41
Allison Rodrigues <allison.ifg@gmail.com>
2021-10-26 10:08:36 -0300

    Importa e Exportar JS

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import{mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs"
+
+console.log(mostraCidade('João', 'Jataí'))
+console.log(mostraHobby('João', 'PC'))
+console.log(mostraIdade('João', 20))
